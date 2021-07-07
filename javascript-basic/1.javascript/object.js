35b801d7ecb960dfbfbf60c2738dcead2450fd12
Sahdat hossen <sahdathossen76@gmail.com>
2021-07-07 12:53:30 +0600

    object js

---
+++ javascript-basic/1.javascript/object.js
@@ -0,0 +1,19 @@
+
+var student1 = {id:588, phone:1980, name:'abrar'};
+var student2 = {id:688, phone:1890, name:'tanbir'};
+
+// var phoneNo = student1.phone;
+
+// var phoneNo = student1["phone"];
+
+var phoneProperty = "phone";
+var phoneNo = student1[phoneProperty];
+
+
+//update value
+// student2.phone = 111254;
+// student2["phone"] = 111254;
+
+console.log(phoneNo);
+console.log(student1);
+console.log(student2);
