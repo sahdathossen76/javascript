8dd51e97347e07d4add955f3347078b71dd13c4c
sahdathossen76 <sahdathossen76@gmail.com>
2021-07-23 17:55:55 +0600

    max number

---
+++ javascript-problems/max.js
@@ -0,0 +1,24 @@
+ var num1 = 1000;
+ var num2 = 5000;
+ var num3 = 400;
+
+//  if(num1 > num2){
+//      if(num1 > num3){
+//         console.log('num1 is bigger');
+//      }
+//      else{
+//         console.log('num3 is bigger');
+//      }
+//  }
+//  else{
+//      if(num2 > num3){
+//         console.log('num2 is bigger');
+//      }
+//      else{
+//         console.log('num3 is bigger')
+//      }
+//  }
+
+var resultMax = Math.max(num1, num2, num3);
+
+console.log('the bigger number is', resultMax);
--- javascript-problems/random.js
+++ javascript-problems/random.js
@@ -1,1 +1,9 @@
-var num = 2;
+var num = 4.5;
+// var result  = Math.floor(num);
+// var result  = Math.ceil(num);
+// var result  = Math.round(num);
+// console.log(result);
+
+var dice = Math.random() * 10;
+var result  = Math.round(dice);
+console.log(result);
